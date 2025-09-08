5c33608fbc4252e26a4bd35bd1b6467d97e5b61f
Tim <tim.hug@digitecgalaxus.ch>
2025-09-08 13:39:05 +0200

    01. Spielfeld aufsetzen

--- GameOfLife/GameOfLife.ConsoleUi/Program.cs
+++ GameOfLife/GameOfLife.ConsoleUi/Program.cs
@@ -4,7 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Conways Game of Life!");
+        Console.WriteLine("Before starting lets set the playing field size");
+        var windowWidth = GetWindowWidth();
+        var windowHeight = GetWindowHeight();
+        var board = new Board(windowWidth, windowHeight);
+        for (var row1 = 0; row1 < board.PlayingField.GetLength(0); row1++)
+        {
+            for (var column = 0; column < board.PlayingField.GetLength(1); column++)
+            {
+                Console.Write(board.PlayingField[row1, column] ? "X  " : " X ");
+            }
+            Console.WriteLine();
+        }
     }
-    
+
+    public static int GetWindowHeight()
+    {
+        while (true)
+        {
+            Console.WriteLine("How tall should the playing field be?");
+            if (int.TryParse(Console.ReadLine(), out var playingFieldSize))
+            {
+                return playingFieldSize;
+            }
+            Console.WriteLine("Please enter a real number.");
+        }
+    }
+
+    public static int GetWindowWidth()
+    {
+        while (true)
+        {
+            Console.WriteLine("How wide should the playing field be?");
+            if (int.TryParse(Console.ReadLine(), out var playingFieldWidth))
+            {
+                return playingFieldWidth;
+            }
+            Console.WriteLine("Please enter a real number.");
+        }
+    }
 }
---
+++ GameOfLife/GameOfLife/Board.cs
@@ -0,0 +1,11 @@
+﻿namespace GameOfLife;
+
+public class Board
+{
+    public bool[,] PlayingField;
+    public Board(int windowWidth, int windowHeight)
+    {
+        PlayingField = new bool[windowHeight, windowWidth];
+    }
+    
+}
