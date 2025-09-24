a028ab2b546c0488c817a440277ee7cde96b34ca
Tim <tim.hug@digitecgalaxus.ch>
2025-09-24 13:34:22 +0200

    Working Game Of Life Web version

--- GameOfLife/GameOfLife.Web/Components/Pages/Game.razor.cs
+++ GameOfLife/GameOfLife.Web/Components/Pages/Game.razor.cs
@@ -17,18 +17,25 @@ public partial class Game
     
     protected override Task OnAfterRenderAsync(bool firstRender)
     {
+        var amountgenerations = 0;
         if (firstRender)
         {
             Console.WriteLine("GamePage loaded");
             board = new Board(GameHeight, GameWidth, GameAliveCellsPercent);
 
+            
             while (board.IsGameAlive())
             {
+                Thread.Sleep(250);
                 board.AdvanceGeneration();
                 StateHasChanged();
+                amountgenerations++;
+                Console.WriteLine($"Current generation {amountgenerations}");
             }
         }
 
+        Console.WriteLine("Finished 🔮");
+        Console.WriteLine($"Total Generations {amountgenerations}");
         return Task.CompletedTask;
     }
 }
